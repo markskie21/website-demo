cea3c9f61756c16d7c799e21fc70f83d14728d62
markskie21 <156207455+markskie21@users.noreply.github.com>
2025-01-28 06:34:08 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+
 let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar')
+let navbar = document.querySelector('.navbar');
+
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x')
-  navbar.classList.toggle ('active');
+  menuIcon.classList.toggle('bx-x'); 
+  navbar.classList.toggle('active'); 
+
 }
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1080: {
+      slidesPerView: 3,
+    },
+  },
+});
